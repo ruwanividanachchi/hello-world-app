ffabab6d58ca64dfe19b1d022fabee34fb15d232
Ruwani Vidanachchi <t3viru00@students.oamk.fi>
2024-10-04 17:07:18 +0300

    Update hello message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World from Fly.io!');
+    res.send('Hello again from Fly.io!');
 });
 
 app.listen(PORT, () => {
